0d8a5edb961decd7b7594069d1d2beff84fc2ffe
Jim Ström <alexius.strom@gmail.com>
2025-02-21 14:43:06 +0100

    Started on mouse coordinate tracker and click tracker. Design improvements.

--- frontend/src/pages/Paint.jsx
+++ frontend/src/pages/Paint.jsx
@@ -1,13 +1,59 @@
-import React, { useState } from "react"
+import React, { useState, useEffect, useRef, useCallback } from "react"
 
 export default function Paint() {
     const [width, setWidth] = useState(400);
     const [height, setHeight] = useState(400);
+    const [coordinates, setCoordinates] = useState([0, 0]);
+    const [palette, setPalette] = useState([]);
+    const canvasRef = useRef();
 
-    return <div className="flex flex-col h-screen">
-        <div className="flex h-1/10">
+
+    let leftCanvasDown = false;
+    let rightCanvasDown = false;
+
+    // When switching to javascript dom, you'll have to be careful not to manipulate dom
+    // React won't be able to react! But if I don't add/remove children it should be fine
+    // Apparently, using useCallback can prevent unnecessary re-renders
+    const onMove = useCallback((event) => {
+        let rect = event.target.getBoundingClientRect();
+        // I get -1 sometimes. That's no good!
+        let x = Math.round(event.clientX - rect.left);
+        let y = Math.round(event.clientX - rect.left);
+        setCoordinates([Math.round(event.clientX - rect.left), Math.round(event.clientY - rect.top)]);
+    }, []);
+
+    const onDown = useCallback((event) => {
+        leftCanvasDown = true;
+    }, []);
+
+    const onRelease = useCallback((event) => {
+        leftCanvasDown = false;
+    }, []);
+
+    useEffect(() => {
+        canvasRef.current.addEventListener("mousemove", onMove);
+        canvasRef.current.addEventListener("mousedown", onDown);
+        canvasRef.current.addEventListener("mouseup", onRelease);
+
+        // Replace later
+        let colors = [];
+
+        for (let i = 0; i < 16 * 16; i++) {
+            colors.push("Some color");
+        }
+        setPalette(colors);
+
+        return () => {
+            canvasRef.current.removeEventListener("mousemove", onMove);
+            canvasRef.current.removeEventListener("mousedown", onDown);
+            canvasRef.current.removeEventListener("mouseup", onRelease);
+        }
+    }, [])
+
+    return <div className="flex flex-col max-h-screen bg-gray-50">
+        <div className="flex rounded-b-lg border-b border-b-gray-300">
             <nav>
-                <ul className="flex ml-3 gap-3">
+                <ul className="flex text-sm ml-3 my-1 gap-3">
                     <li>File</li>
                     <li>Edit</li>
                     <li>View</li>
@@ -15,7 +61,7 @@ export default function Paint() {
                 </ul>
             </nav>
         </div>
-        <div className="flex flex-row h-1/10">
+        <div className="flex flex-row border-b border-b-gray-300">
             <nav>
                 <ul className="flex flex-row text-xs p-1 gap-1 border border-gray-100">
                     <li>Pencil</li>
@@ -25,7 +71,7 @@ export default function Paint() {
                 </ul>
             </nav>
             <nav>
-                <ul className="flex flex-row text-xs p-1 gap-1 border border-gray-100">
+                <ul className="flex flex-row text-xs p-1 gap-1 border-gray-100">
                     <li>Line</li>
                     <li>Rectangle</li>
                     <li>Fill rectangle</li>
@@ -55,19 +101,21 @@ export default function Paint() {
                 </ul>
             </nav>
         </div>
-        <div className="flex h-7/10">
-            <div className="flex flex-col items-center w-[80%] overflow-scroll">
-                <div className="bg-white w-[460px] min-h-[60px]"></div>
+        {/* Needed min-h-full to lower height and enforce scroll */}
+        <div className="flex grow min-h-full">
+            <div className="flex flex-col grow items-center overflow-scroll bg-gray-300">
+                <div className="bg-gray-300 w-[460px] min-h-[60px]"></div>
                 <canvas
+                    ref={canvasRef}
                     width={width}
                     height={height}
-                    className="bg-amber-200 w-[400px] h-[400px]">
+                    className="bg-white w-[400px] h-[400px]">
                 </canvas>
-                <div className="bg-white w-[460px] min-h-[60px]"></div>
+                <div className="bg-gray-300 w-[460px] min-h-[60px]"></div>
             </div>
-            <div className="flex flex-col w-[20%]">
-                <div className="grid">
-                    Palette
+            <div className="flex flex-col">
+                <div className="grid grid-cols-16 gap-0 m-2">
+                    {palette.map((color) => <div className="size-3 bg-red-800 border border-t-gray-800 border-l-gray-800 border-b-gray-300 border-r-gray-300"></div>)}
                 </div>
                 <div className="flex">
                     Active colors
@@ -77,8 +125,10 @@ export default function Paint() {
                 </div>
             </div>
         </div>
-        <div className="flex h-1/10">
-            Status bar
+        <div className="flex flex-row-reverse text-sm border-t border-t-gray-300">
+            <div className="flex m-1">
+                ({coordinates[0]}, {coordinates[1]})
+            </div>
         </div>
     </div>
 }
