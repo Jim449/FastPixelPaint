6a94df002fd6db3e8bc404d581bccf9891644ec8
Jim Ström <alexius.strom@gmail.com>
2025-02-23 15:22:26 +0100

    Added geometry module. Tested out some drawing methods.

--- frontend/src/pages/Paint.jsx
+++ frontend/src/pages/Paint.jsx
@@ -7,15 +7,17 @@ export default function Paint() {
     const [coordinates, setCoordinates] = useState([0, 0]);
     const [palette, setPalette] = useState([]);
     const [menu, setMenu] = useState("");
-    // const [menuOptions, setMenuOptions] = useState();
 
     const canvasRef = useRef();
+    const overlayRef = useRef();
     const docRef = useRef(document);
     // Had to make this a ref, or it wouldn't update
     const menuOpen = useRef(false);
 
     let canvasLeftDown = false;
     let canvasRightDown = false;
+    let shiftDown = false;
+    let startPoint = [0, 0];
     let lastPoint = [0, 0];
     let currentPoint = [0, 0];
     let loopId;
@@ -36,8 +38,6 @@ export default function Paint() {
         { label: "Open with current palette", type: "button", action: () => { } },
         { label: "Open text editor", type: "button", action: () => { } }
     ];
-
-    // { label: "Export", type: "anchor", url: canvasRef.current.toDataURL("image/png"), download: "Image" },
 
     const editOptions = [
         { label: "Undo", type: "button", action: () => { } },
@@ -73,6 +73,7 @@ export default function Paint() {
     }
 
     function onMenuOpen(event) {
+        // Toggles dropdown menu
         if (menu == event.target.id) {
             setMenu("");
             menuOpen.current = false;
@@ -81,24 +82,91 @@ export default function Paint() {
             setMenu(event.target.id);
             menuOpen.current = true;
         }
-
     }
+
     // Drawing in here rather than in mousemove event should be cheaper
     function drawingLoop(time) {
         // A drawing loop, called at 60 FPS or so
         if (canvasLeftDown) {
-            const context = canvasRef.current.getContext("2d");
+            // const context = canvasRef.current.getContext("2d");
+
+            // Draws multiple lines to make for a "pencil"
             // context.strokeStyle = "black";
             // context.lineWidth = 1;
             // context.moveTo(lastPoint[0], lastPoint[1]);
             // context.lineTo(currentPoint[0], currentPoint[1]);
             // context.closePath();
             // context.stroke();
-            // I could draw a line but it's too smooth, try this instead 
+            // This is all well and good but it's smooth
+            // It isn't what I'd call pixel art
+
+            // Draws small rectangles to make for a "dot tool"
+            // context.fillStyle = "black";
+            // context.fillRect(currentPoint[0], currentPoint[1], 1, 1);
+
+            // This is a preview placed on top of the regular canvas
+            const context = overlayRef.current.getContext("2d");
+            context.clearRect(0, 0, width, height);
+            // If you don't start a new path, clearRect fails
+            context.beginPath();
+
+            // Draws a line
+            // context.strokeStyle = "black";
+            // context.lineWidth = 1;
+            // context.moveTo(startPoint[0], startPoint[1]);
+            // context.lineTo(currentPoint[0], currentPoint[1]);
+            // context.closePath();
+            // context.stroke();
+
+            let startX;
+            let startY;
+            let dx;
+            let dy;
+
+            // Move this to a function. Use the geometry module
+            if (currentPoint[0] > startPoint[0]) {
+                startX = startPoint[0];
+                dx = currentPoint[0] - startPoint[0];
+            }
+            else {
+                startX = currentPoint[0];
+                dx = startPoint[0] - currentPoint[0];
+            }
+
+            if (currentPoint[1] > startPoint[1]) {
+                startY = startPoint[1];
+                dy = currentPoint[1] - startPoint[1];
+            }
+            else {
+                startY = currentPoint[1];
+                dy = startPoint[1] - currentPoint[1];
+            }
+
+            // Draws a rectangle outline
+            // context.strokeStyle = "black";
+            // context.lineWidth = 1;
+            // context.strokeRect(startX + 0.5, startY + 0.5, dx, dy);
+            // Nice! Doesn't look too black, though. Just gray
+            // It looks good if I offset to x+0.5 and y+0.5
+
+            // Draws a solid rectangle
+            // context.fillStyle = "black";
+            // context.lineWidth = 1;
+            // context.fillRect(startX, startY, dx, dy);
+            // Very nice!
+
+            // Draws an ellipse outline
+            // context.strokeStyle = "black";
+            // context.lineWidth = 1;
+            // context.ellipse((startPoint[0] + currentPoint[0]) / 2, (startPoint[1] + currentPoint[1]) / 2, dx / 2, dy / 2, 0, 0, Math.PI * 2);
+            // context.stroke();
+            // Nice! Not really pixelart so I need another approach
+
+            // Draws a solid ellipse
             context.fillStyle = "black";
-            context.fillRect(currentPoint[0], currentPoint[1], 1, 1);
-            // This kind of dot pencil isn't too bad
-            // Why not add a tool for it?
+            context.ellipse((startPoint[0] + currentPoint[0]) / 2, (startPoint[1] + currentPoint[1]) / 2, dx / 2, dy / 2, 0, 0, Math.PI * 2);
+            context.fill();
+            // Nice!
         }
         lastPoint = [...currentPoint];
         if (coordinates[0] != currentPoint[0] || coordinates[1] != currentPoint[1]) {
@@ -131,8 +199,12 @@ export default function Paint() {
             setMenu("");
             menuOpen.current = false;
         }
-        if (id === "canvas") {
+        if (id === "overlay") {
+            let rect = canvasRef.current.getBoundingClientRect();
+            let x = Math.round(event.clientX - rect.left);
+            let y = Math.round(event.clientY - rect.top);
             canvasLeftDown = true;
+            startPoint = [x, y];
         }
     }, []);
 
@@ -262,13 +334,24 @@ export default function Paint() {
         <div className="flex grow min-h-full">
             <div className="flex flex-col grow items-center overflow-scroll bg-gray-300">
                 <div className="bg-gray-300 w-[360px] min-h-[60px]"></div>
-                <canvas
-                    id="canvas"
-                    ref={canvasRef}
-                    width={width}
-                    height={height}
-                    className="w-[300px] h-[300px]">
-                </canvas>
+                <div
+                    className="relative w-[300px] h-[300px]">
+                    <canvas
+                        id="canvas"
+                        ref={canvasRef}
+                        width={width}
+                        height={height}
+                        className="absolute top-0 left-0 w-[300px] h-[300px] z-0">
+                    </canvas>
+                    <canvas
+                        id="overlay"
+                        ref={overlayRef}
+                        width={width}
+                        height={height}
+                        className="absolute top-0 left-0 w-[300px] h-[300px] z-10">
+                    </canvas>
+                </div>
+
                 <div className="bg-gray-300 w-[360px] min-h-[60px]"></div>
             </div>
             <div className="flex flex-col">
---
+++ frontend/src/scripts/geometry.js
@@ -0,0 +1,106 @@
+export function getHorizontalLine(x1, y1, dx) {
+    // Draws a horizontal line from (x1, y1) with length dx
+    let coordinates = [];
+
+    for (x = x1; x <= x1 + dx; x++) {
+        coordinates.push([x, y1]);
+    }
+    return coordinates;
+}
+
+
+export function getVerticalLine(x1, y1, dy) {
+    // Draws a vertical line from (x1, y1) with length dy
+    let coordinates = [];
+
+    for (y = y1; y <= y1 + dy; y++) {
+        coordinates.push([x1, y]);
+    }
+    return coordinates;
+}
+
+
+function getXLine(x1, y1, dx, slope) {
+    // Returns a line starting at (x1, y1), proceeding dx steps in x-direction with given y-slope
+
+    if (slope == 0) {
+        return getHorizontalLine(x1, y1, dx);
+    }
+    let fraction = slope / dx;
+    let coordinates = [[x1, y1]];
+
+    for (let step = 1; step <= dx; step++) {
+        coordinates.push([x + step, Math.round(y1 + step * fraction)]);
+    }
+    return coordinates;
+}
+
+
+function getYLine(x1, y1, dy, slope) {
+    // Returns a line starting at (x1, y1), proceeding dy steps in y-direction with given x-slope
+    if (slope == 0) {
+        return getVerticalLine(x1, y1, dy);
+    }
+    let fraction = slope / dy;
+    let coordinates = [[x1, y1]];
+
+    for (let step = 1; step <= dy; step++) {
+        coordinates.push([Math.round(x1 + step * fraction), y + step]);
+    }
+    return coordinates;
+}
+
+
+export function getLine(x1, y1, x2, y2) {
+    // Draws a line between two points
+    let dx = x2 - x1;
+    let dy = y2 - y1;
+
+    if (Math.abs(dx) > Math.abs(dy)) {
+        let slope = dy / dx;
+
+        if (dx > 1) {
+            return getXLine(x1, y1, dx, slope);
+        }
+        else {
+            return getXLine(x2, y2, -dx, slope);
+        }
+    }
+    else {
+        let slope = dx / dy;
+
+        if (dy > 1) {
+            return getYLine(x1, y1, dy, slope);
+        }
+        else {
+            return getYLine(x2, y2, -dy, slope);
+        }
+    }
+}
+
+
+export function getStraightLine(x1, y1, x2, y2) {
+    // Given two points (x1, y1) and (x2, y2), 
+    // draws a horizontal line to (x2, y1)
+    // or a vertical line to (x1, y2),
+    // whichever has the weakest slope
+    let dx = x2 - x1;
+    let dy = y2 - y1;
+
+    if (Math.abs(dx) > Math.abs(dy)) {
+        if (dx > 1) {
+            return getHorizontalLine(x1, y1, dx);
+        }
+        else {
+            return getHorizontalLine(x2, y1, -dx);
+        }
+    }
+    else {
+        if (dy > 1) {
+            return getVerticalLine(x1, y1, dy);
+        }
+        else {
+            return getVerticalLine(x1, y2, -dy);
+        }
+    }
+}
